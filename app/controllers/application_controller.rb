8b62101e14175aec097501f339d763180ed7cb33
Christopher Araujo <78472544+dcaaraujo@users.noreply.github.com>
2024-02-14 00:22:23 +0200

    Added Spanish locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :set_i18n_locale_from_params
   before_action :authorize
 
   protected
@@ -7,5 +8,16 @@ class ApplicationController < ActionController::Base
       unless User.exists?(session[:user_id])
         redirect_to login_url, notice: "Please log in"
       end
+    end
+
+    def set_i18n_locale_from_params
+      new_locale = params[:locale]
+      return if new_locale.nil?
+      if I18n.available_locales.map(&:to_s).include?(new_locale)
+        I18n.locale = new_locale
+      else
+        flash.now[:notice] = "#{new_locale} translation not available"
+        logger.error flash.now[:notice]
+      end        
     end
 end
